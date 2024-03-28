125b7927cc0d6e4dfe8399979a8df71fd2bf420d
Victor Caus <victorcauzz@hotmail.com>
2024-03-28 19:09:51 -0300

    Lotes importados e armazenados

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 import constants as c
 from enemy import Enemy
 from world import World
+from turret import Turret
 
 
 #initialise pygame
@@ -21,20 +22,28 @@ pg.display.set_caption("ITAwer Defense")
 enemy_image = pg.image.load("./enemy_1.png")
 map_image = pg.image.load("./assets/mapa/mapa4.png")
 map_image_resized = pg.transform.scale(map_image, (screen.get_width(), screen.get_height()))
+#individual turret image for mouse cursor
+cursor_turret = pg.image.load("./assets/turrets/cursor_turret.png").convert_alpha()
 
 #load json data for level
 with open('assets/mapa/mapaTiled/level_data.tmj') as file:
     world_data = json.load(file)
 
+def create_turret(mouse_pos):
+    mouse_tile_x = mouse_pos[0] // c.TILE_SIZE
+    turret = Turret(cursor_turret, mouse_pos)
+    turret_group.add(turret)
+
+
 # Create world
 world = World(screen, world_data, map_image_resized)
-world.process_data()
-
 
 
 # Enemies groups
 enemy_group = pg.sprite.Group()
-enemy = Enemy(world.paths[0], enemy_image)
+turret_group = pg.sprite.Group()
+
+enemy = Enemy(world.paths[5], enemy_image)
 enemy_group.add(enemy)
 
 # Game loop
@@ -50,19 +59,28 @@ while run:
     world.draw(screen)
 
     #draw enemy path
-    pg.draw.lines(screen, "grey0", False, world.paths[0])
+    pg.draw.lines(screen, "grey0", False, world.paths[5])
 
     #update groups
     enemy_group.update()
 
-    #draw enemies group
+    #draw groups
     enemy_group.draw(screen)
+    turret_group.draw(screen)
 
     #event handler
     for event in pg.event.get():
         #quit program
         if event.type == pg.QUIT:
             run = False
+
+        # Mouse Click:
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pg.mouse.get_pos()
+            # Check if mouse is on the game area
+            if mouse_pos[0] < screen.get_width() and mouse_pos[1] < screen.get_height():
+                create_turret(mouse_pos)
+
 
     #update the display
     pg.display.flip()
--- world.py
+++ world.py
@@ -1,4 +1,42 @@
 import pygame as pg
+from enum import Enum
+
+
+class PlotStates(Enum):
+    """
+    Um enum que serve para abstrair o estados de um lote
+    """
+    FOR_SALE = 0
+    FREE = 1
+    OCCUPIED = 2
+
+
+class Plot:
+    """
+    Uma classe que representa um lote onde vai ser colocado as torres
+    Args:
+        obj: O objeto do arquivo de dados do level json
+
+    Attributes:
+        x_left: Coordenada x do lado esquerdo
+        x_right: Coordenada x do lado direito
+        y_top: Coordenada y do lado de cima
+        y_bottom: Coordenada y do lado de baixo
+        state: Estado do lote (FOR_SALE, FREE, OCCUPIED)
+    """
+
+    def __init__(self, obj, x_ratio, y_ratio):
+        self.x_left = obj['x'] * x_ratio
+        self.x_right = (obj['x'] + obj['width']) * x_ratio
+        self.y_top = obj['y'] * y_ratio
+        self.y_bottom = (obj['y'] + obj['height']) * y_ratio
+        self.state = PlotStates.FOR_SALE
+
+    def is_in(self, pos):
+        return self.x_left <= pos[0] <= self.x_right and self.y_top <= pos[1] <= self.y_bottom
+
+    def center(self):
+        return (self.x_left + self.x_right) / 2, (self.y_top + self.y_bottom) / 2
 
 
 class World:
@@ -10,12 +48,14 @@ class World:
      Attributes:
          image: A imagem do mapa.
      """
-    def __init__(self,screen, data, map_image):
+    def __init__(self, screen, data, map_image):
         self.paths = []
+        self.plots = []
         self.level_data = data
         self.image = map_image
         self.x_ratio = screen.get_width()/(data['width']*data['tilewidth'])
         self.y_ratio = screen.get_height()/(data['height']*data['tileheight'])
+        self.process_data()
 
     def process_data(self):
         """
@@ -25,6 +65,10 @@ class World:
             if "path" in layer["name"]:
                 for obj in layer["objects"]:
                     self.process_waypoints(obj["polyline"], obj["x"], obj["y"])
+            elif "lote" in layer["name"]:
+                for obj in layer["objects"]:
+                    # Armazena as posições corrigidas dos lotes:
+                    self.plots.append(Plot(obj, self.x_ratio, self.y_ratio))
 
     def process_waypoints(self, points, abs_x, abs_y):
         """
@@ -40,6 +84,7 @@ class World:
             temp_y = (abs_y + point.get("y")) * self.y_ratio
             waypoints.append((temp_x, temp_y))
         self.paths.append(waypoints)
+
     def draw(self, surface):
         """
          Desenha a imagem do mundo numa superfície.
