b2b1214f5699bc410f7891460d7246a09c07efb4
gabrielcbomfim <gabrielcaetite@hotmail.com>
2024-04-03 13:47:25 -0300

    Turrets

--- constants.py
+++ constants.py
@@ -1,7 +1,12 @@
 # Constants used in the game
 
 SCREEN_HEIGHT = 800
-SCREEN_WIDHT = 1600
-TILE_SIZE = 55
+SCREEN_WIDHT = 1200
+TILE_SIZE = 1
 FPS = 60
 
+#turret constants
+
+ANIMATION_STEPS = 8
+ANIMATION_DELAY = 15
+TURRET_LEVELS = 4
